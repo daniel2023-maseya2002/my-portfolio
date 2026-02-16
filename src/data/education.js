261d0edefb443774f3b7e3196a3a667ebd69ec7c
daniel2023-maseya2002 <maseyadaniel@gmail.com>
2026-02-16 02:22:43 +0200

    Added the new certicates

--- src/data/education.js
+++ src/data/education.js
@@ -1,11 +1,19 @@
 // src/data/education.js
+import alxLogo from "../assets/logos/AlxLogo.png";
 import aucaLogo from "../assets/logos/auca-logo.png";
+import ciscologo from "../assets/logos/ciscologo.png";
 import figmaLogo from "../assets/logos/figma-logo.png";
+import freecodecamplogo from "../assets/logos/freecodecamp.jpg";
 import highschoolLogo from "../assets/logos/highschool-logo.png";
 
 import aucaTranscript from "../assets/transcripts/auca-transcript.pdf";
-import figmaCertificate from "../assets/transcripts/figma-certificate.pdf";
+import figmaCertificate from "../assets/transcripts/figma-certificate.png";
 import highschoolTranscript from "../assets/transcripts/highschool-transcript.pdf";
+import internet from "../assets/transcripts/internet-society.pdf";
+import javascript from "../assets/transcripts/javascript.png";
+import va from "../assets/transcripts/VA.png";
+import web from "../assets/transcripts/web.png";
+
 
 const education = [
   {
@@ -55,6 +63,70 @@ const education = [
     type: "certificate",
     highlights: [
       "Hands-on projects: Design systems, responsive prototypes"
+    ]
+  },
+  {
+    id: "cert-figma-2025",
+    institution: "Alx Africa",
+    degree: "Certificate",
+    field: "Virtual Assistance",
+    period: "2025",
+    location: "Online",
+    gpa: null,
+    honors: null,
+    logo: alxLogo,
+    transcripts: va,
+    type: "certificate",
+    highlights: [
+      "For successsfully completing an 8-weeks programme in VA Skills in the Digital Age."
+    ]
+  },
+   {
+    id: "cert-figma-2025",
+    institution: "FreeCodeCamp",
+    degree: "Certificate",
+    field: "Responsive Web Design",
+    period: "2025",
+    location: "Online",
+    gpa: null,
+    honors: null,
+    logo: freecodecamplogo,
+    transcripts: web,
+    type: "certificate",
+    highlights: [
+      "Representing approximately 300 hours of works"
+    ]
+  },
+  {
+    id: "cert-figma-2026",
+    institution: "FreeCodeCamp",
+    degree: "Certificate",
+    field: "JavaScript",
+    period: "2026",
+    location: "Online",
+    gpa: null,
+    honors: null,
+    logo: freecodecamplogo,
+    transcripts: javascript,
+    type: "certificate",
+    highlights: [
+      "Representing approximately 300 hours of works"
+    ]
+  },
+  {
+    id: "cert-figma-2024",
+    institution: "Cisco Network",
+    degree: "NDG LINUX SERIES",
+    field: "JavaScript",
+    period: "2025",
+    location: "Online",
+    gpa: null,
+    honors: null,
+    logo: ciscologo,
+    transcripts: internet,
+    type: "certificate",
+    highlights: [
+      "Become a stronger prospect in the job market by pairing our certificate with other industry-recognized certifications such as Cisco Certified Network Associate (CCNA"
     ]
   }
 ];
